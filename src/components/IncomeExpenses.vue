2bdbbc4f8a5491ad28258be307f92b6e08003c81
Ruthbaby <iroanusi.ruth@gmail.com>
2025-03-17 17:12:26 +0100

    Added Styling

--- src/components/IncomeExpenses.vue
+++ src/components/IncomeExpenses.vue
@@ -4,9 +4,10 @@
       <h4>Income</h4>
       <p id="money-plus" class="money plus">+${{income}}</p>
     </div>
-  </div>
+  
   <h4>Expense</h4>
   <p id="money-minus" class="money minus">-${{expenses}}</p>
+</div>
 </template>
 
 <script setup>
